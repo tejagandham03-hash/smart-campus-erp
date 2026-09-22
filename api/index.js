420777d91236aaec5495979a67baab691dded4b8
tejagandham03 <tejagandham03@gmail.com>
2026-09-22 14:52:33 +0530

    msg

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: './server/.env' });
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../server/.env') });
 
 const app = require('../server/src/app');
 const connectDB = require('../server/src/config/database');
